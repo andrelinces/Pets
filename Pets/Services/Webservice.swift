a9385f29f3cf415c77c525b7ba1e163029dff76d
andrelinces <andrelinces@yahoo.com.br>
2024-10-16 22:28:40 -0300

    feat Created app Pets. Created app Pet and Webservices class.

---
+++ Pets/Services/Webservice.swift
@@ -0,0 +1,31 @@
+//
+//  Webservice.swift
+//  Pets
+//
+//  Created by Andre Linces on 10/16/24.
+//
+
+import Foundation
+
+enum NetWorkError: Error {
+    case invalidUrl
+    case invalidServerResponse
+}
+
+class Webservice {
+    
+    
+    func load(resource: String) async throws {
+        
+        guard let url = URL(string: resource) else {
+            throw NetWorkError.invalidUrl
+        }
+       let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+           throw NetWorkError.invalidServerResponse
+        }
+    }
+    
+}
---
+++ Pets/Utils/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Pets
+//
+//  Created by Andre Linces on 10/16/24.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Urls {
+        static let baseUrl = "http://localhost:3000/"
+        static let petListing = "\(baseUrl)/pet-listing"
+    }
+    
+    
+}
