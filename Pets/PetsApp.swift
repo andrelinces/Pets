c1e7481c93bda2d718381fcc7455df2ac64a1569
andrelinces <andrelinces@yahoo.com.br>
2024-10-16 22:00:27 -0300

    Initial Commit

---
+++ Pets/PetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsApp.swift
+//  Pets
+//
+//  Created by Andre Linces on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
